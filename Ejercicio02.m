4a5df560d3d1d9cbf928e814d8d4496c201ecbcc
LIVEU-TAD\eugenio.salguero <eugenio.salguero@live.u-tad.com>
2018-01-13 12:08:07 +0100

    no message

--- Ejercicio02.m
+++ Ejercicio02.m
@@ -1,4 +1,5 @@
 for N = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000]
+  
   valorTotal = 0;
   
   for m = 0:N
@@ -12,7 +13,10 @@ for N = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000]
     
   end
   
-  valorTotal
+  
+  valorPi = valorTotal
+  
+  valorError = abs(valorPi - pi)
   
 end
            
