539d323f360c16020fff69447a83d56dd74006a1
essalguero <essalguero@yahoo.es>
2018-02-10 10:37:31 +0100

    Add files via upload

---
+++ Ejercicio05.m
@@ -0,0 +1,31 @@
+clear
+clc
+
+format long
+
+f = @(x, y) 8 * pi^2 * sin(2 * pi .* x) * cos(2 * pi .* y)
+DeltaU = f;
+h1 = 1 / 10;
+h2 = 1 / 20;
+
+a = 0; b = 1; c = 0; d = 1;
+
+bound = @(x) sin(2 * pi * x)
+
+uex = @(x, y) sin(2 * pi * x) * cos(2 * pi * y)
+
+[u1, x1, y1, error1] = poissonfd(a, c, b, d, (b - a) / h1, (b - a) / h1, f, bound, uex);
+[u2, x2, y2, error2] = poissonfd(a, c, b, d, (b - a) / h2, (b - a) / h2, f, bound, uex);
+
+u1
+u2
+
+printf("Error1: %f\n", error1);
+printf("Error2: %f\n", error2);
+
+figure
+mesh(x1, y1, u1);
+figure
+mesh(x2, y2, u2);
+
+%uex(x1, y1) - error1
